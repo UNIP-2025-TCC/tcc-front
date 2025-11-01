03b3c69a8185d2b819eaecdaf656c5a6a0d81814
Fernando Soares <nafferweb@proton.me>
2025-11-01 16:51:58 -0300

    Style: Criação do Layout da aplicação e início da página principal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import '@/global/globals.css';
 import { inter } from '@/assets/fonts';
 import { HeroUiProvider } from '@/global/HeroUiProvider';
+import Image from 'next/image';
 
 export const metadata: Metadata = {
     title: 'TCC FRONT',
@@ -15,9 +16,14 @@ export default function LoginLayout({
     return (
         <html lang="pt-BR" suppressHydrationWarning>
             <body
-                className={`antialiased ${inter.className} max-w-full h-[100svh]`}
+                className={`antialiased ${inter.className} max-w-full h-[100svh] bg-gradient-to-br from-white to-amber-50`}
             >
-                <HeroUiProvider>{children}</HeroUiProvider>
+                <div id='menu-header' className='w-full px-16 py-4 bg-white mb-6'>
+                    <Image src="/img/mega_logo.png" alt="Logo" width={48} height={48} />
+                </div>
+                <div className='max-w-[1200px] mx-auto bg-white/80 rounded-2xl border-1 border-stone-100 p-6'>
+                    <HeroUiProvider>{children}</HeroUiProvider>
+                </div>
             </body>
         </html>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import { Button } from '@/lib/heroui';
 
 export default function Home() {
-    return <Button>Botão</Button>;
+    return (
+        <div >
+            <h1 className='text-3xl font-bold text-stone-900 mb-1'>Bem-vindo ao <span className='text-amber-500'>MEGA</span>!</h1>
+            <p className='text-stone-500 text-xl'>Selecione uma câmera para visualizar ou acesse as estatísticas gerais</p>
+            <div>
+
+            </div>
+        </div>
+    )
 }
